a5b49629144d1f32c0cccf4dcb3ee47696c04ef6
amirhoseinhoseinpoor <127256622+amirhoseinhoseinpoor@users.noreply.github.com>
2023-11-01 04:18:05 +0330

    Add files via upload

---
+++ telegrambot 2/telegrambot 2/Program.cs
@@ -0,0 +1,81 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+var botClient = new TelegramBotClient("6803216014:AAEDeyqRunc4Lyp9eCnlj9hdyU3-XnxwZK4");
+using CancellationTokenSource cts = new();
+CryptoPriceFetcher cryptoPriceFetcher = new CryptoPriceFetcher();
+var (usdPrice, jpyPrice, eurPrice) = await CryptoPriceFetcher.GetBitcoinPricesAsync();
+
+ReceiverOptions receiverOptions = new()
+{
+    AllowedUpdates = Array.Empty<UpdateType>()
+};
+
+botClient.StartReceiving(
+updateHandler: HandleUpdateAsync,
+pollingErrorHandler: HandlePollingErrorAsync,
+receiverOptions: receiverOptions,
+cancellationToken: cts.Token
+);
+
+var me = await botClient.GetMeAsync();
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+cts.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    if (update.Message is not { } message)
+        return;
+    if (message.Text is not { } messageText)
+        return;
+
+    var chatId = message.Chat.Id;
+    Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+    if (message.Text == "/start")
+    {
+        Message messages = await botClient.SendTextMessageAsync(
+        chatId: chatId,
+        text: "Welcome to the currency Bot! Type '/usdPrice', '/jpyPrice', or '/eurPrice' to get the respective currency prices.",
+        cancellationToken: cancellationToken);
+
+    }
+    else if (message.Text == "/usdPrice")
+    {
+        Message messages = await botClient.SendTextMessageAsync(
+       chatId: chatId,
+       text: $"Current USD price is : {usdPrice}",
+       cancellationToken: cancellationToken);
+    }
+    else if (message.Text == "/eurPrice")
+    {
+        Message messages = await botClient.SendTextMessageAsync(
+       chatId: chatId,
+       text: $"Current eur price is : {eurPrice}",
+       cancellationToken: cancellationToken);
+    }
+    else if (message.Text == "/jpyPrice")
+    {
+        Message messages = await botClient.SendTextMessageAsync(
+       chatId: chatId,
+       text: $"Current jpy price is : {jpyPrice}",
+       cancellationToken: cancellationToken);
+    }
+
+}
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
+
---
+++ telegrambot 2/telegrambot 2/api rader.cs
@@ -0,0 +1,28 @@
+﻿public class CryptoPriceFetcher
+{
+
+    private static readonly HttpClient HttpClient = new HttpClient();
+    private static readonly string ApiUrl = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR";
+
+    public static async Task<(double usdPrice, double jpyPrice, double eurPrice)> GetBitcoinPricesAsync()
+    {
+        HttpResponseMessage response = await HttpClient.GetAsync(ApiUrl);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+            dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(jsonResponse);
+
+            double usdPrice = data?.USD;
+            double jpyPrice = data?.JPY;
+            double eurPrice = data?.EUR;
+
+            return (usdPrice, jpyPrice, eurPrice);
+        }
+        else
+        {
+            Console.WriteLine("Error fetching data from the API");
+            return (0, 0, 0);
+        }
+    }
+}
